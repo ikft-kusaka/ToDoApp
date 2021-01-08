0beb3874301373352077a071ecd42b3ed4421630
ikft-kusaka <kusaka.ryu@ik-ft.com>
2021-01-08 11:44:26 +0900

    chapter4 finished

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -3,19 +3,28 @@
 namespace App\Http\Controllers;
 
 use App\Models\Folder;
+use App\Models\Task;
 use Illuminate\Http\Request;
 
 class TaskController extends Controller
 {
     public function index(int $id)
     {
+        // 全てのフォルダを取得
         $folders = Folder::all();
+
+        // 選ばれたフォルダを取得する find()プライマリキーのカラムを条件に1行分のデータを取得する
+        $current_folder = Folder::find($id);
+
+        // 選ばれたフォルダに紐づくタスクを取得する　Tasks::where('folder_id', '=', $current_folder->id)->get();
+        $tasks = $current_folder->tasks()->get();
 
         // view取得したデータをリターンする
         return view('tasks/index', [
             // テンプレート側ではキー名が変数名となる
             'folders' => $folders,
-            'current_folder_id' => $id,
+            'current_folder_id' => $current_folder->id,
+            'tasks' => $tasks,
         ]);
     }
 }
--- app/Models/Folder.php
+++ app/Models/Folder.php
@@ -7,5 +7,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Folder extends Model
 {
-    use HasFactory;
+    public function tasks()
+    {
+        // フォルダクラスのインスタンスから紐づくタスククラスのリストを取得
+        // 省力せずに書くと$this->hasMany('App\Task', 'folder_id', 'id');
+        return $this->hasMany('App\Models\Task');
+    }
 }
